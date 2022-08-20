c90ac2686e06f5e9e0bcf7d77283bf9baabc0dbe
Garth <garthcline@gmail.com>
2022-08-20 01:48:43 -0400

    Adding some demo files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("teasing")
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.listen(3000,() =>{
+ console.log('listening')
+});
