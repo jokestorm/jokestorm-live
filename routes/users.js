bc452b8ef3b8ccbb6811a4cec4fe53575dfb2694
Garth <garthcline@gmail.com>
2022-12-05 22:52:03 -0500

    Basic signup post

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const catchAsync = require('../utils/catchAsync');
 const router = express.Router();
 const User = require('../models/user');
 
 router.get('/signup', (req, res) => {
     res.render('users/signup');
-})
+});
 
-router.post('/signup', async(req, res) => {
-    res.send(req.body);
-})
+router.post('/signup', catchAsync(async (req, res) => {
+
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
+        req.flash('success', 'Great Job Signing Up!');
+        res.redirect('/members');
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/signup')
+    }
+}));
+
 module.exports = router;
