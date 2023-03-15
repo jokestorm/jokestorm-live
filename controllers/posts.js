b4b6428f99e7ac2a49f635b6e03ab15b836a09ed
Garth <garthcline@gmail.com>
2023-03-14 23:21:28 -0400

    delete images

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const posts = await Post.find({});
@@ -12,7 +13,7 @@ module.exports.renderNewForm = (req, res) => {
 module.exports.createPost = async (req, res, next) => {
     const post = new Post(req.body.post);
     post.author = req.user._id;
-    post.images = req.files.map(f => ({ url: f.path, filename: f.filename}));
+    post.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     await post.save();
     req.flash('success', 'Successfully submitted a new post!');
     res.redirect(`/posts/${post._id}`);
@@ -38,7 +39,6 @@ module.exports.renderEditForm = async (req, res) => {
         req.flash('error', 'Unable to find post');
         return res.redirect('/posts');
     }
-    console.log(post)
     res.render('posts/edit', { post });
 }
 
@@ -51,11 +51,16 @@ module.exports.deletePost = async (req, res) => {
 
 module.exports.editPost = async (req, res) => {
     const { id } = req.params;
-    console.log(req.body)
     const post = await Post.findByIdAndUpdate(id, { ...req.body.post });
-    const imgs =req.files.map(f => ({ url: f.path, filename: f.filename}));
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     post.images.push(...imgs);
     await post.save();
+    if (req.body.deleteImages) {
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename);
+        }
+        await post.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
     req.flash('success', 'Successfully updated.');
     res.redirect(`/posts/${post._id}`);
 }
